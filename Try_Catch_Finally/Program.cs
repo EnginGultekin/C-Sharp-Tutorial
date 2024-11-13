77c932d77ff00597f86febb36e7bc838b1eca5b0
Engin Gültekin <engingultekin1071@gmail.com>
2024-11-13 09:48:16 +0300

    Try-Catch-Finally tutorial file added

---
+++ Try_Catch_Finally/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace TryCatchFinallyExample
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            //  func1();
+            try
+            {
+                Console.WriteLine("Bir sayı giriniz:");
+                string input = Console.ReadLine();
+                int number = Convert.ToInt32(input);
+                Console.WriteLine($"Girilen sayının karesi: {number * number}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Geçersiz format! Lütfen bir sayı giriniz.");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Girilen sayı çok büyük veya çok küçük!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Bir hata oluştu: {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Try-Catch bloğu sona erdi. Program sonlandırılıyor.");
+            }
+        }
+
+
+        public static void func1()
+        {
+            try
+            {
+                Console.WriteLine("Lütfen birinci sayıyı giriniz:");
+                int a = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Lütfen ikinci sayıyı giriniz:");
+                int b = int.Parse(Console.ReadLine());
+
+
+                int c = a + b;
+
+                Console.WriteLine($"Girilen sayıların toplamı: {c}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Bir Hata Oluştu: " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("İşlem tamamlandı.");
+            }
+        }
+    }
+}
