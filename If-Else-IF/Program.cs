d2900b0a84a4dd2677718cca3a68e893f982180b
Engin Gültekin <engingultekin1071@gmail.com>
2024-11-13 16:19:10 +0300

    If else if tutorial added

---
+++ If-Else-IF/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        Ternary();
+        Console.WriteLine("Lütfen bir sayı giriniz:");
+        int number = int.Parse(Console.ReadLine());
+
+        if (number > 0)
+        {
+            Console.WriteLine("Girilen sayı pozitiftir.");
+        }
+        else if (number < 0)
+        {
+            Console.WriteLine("Girilen sayı negatiftir.");
+        }
+        else
+        {
+            Console.WriteLine("Girilen sayı sıfırdır.");
+        }
+    }
+
+
+      static void Ternary()
+    {
+        Console.WriteLine("Lütfen bir sayı giriniz:");
+        int number = int.Parse(Console.ReadLine());
+
+        // Ternary If kullanımı
+        string result = (number % 2 == 0) ? "Girilen sayı çifttir." : "Girilen sayı tektir.";
+        Console.WriteLine(result);
+    }
+}
